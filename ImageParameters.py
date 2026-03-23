6e7942a6bd63156d502993c8faf4d4d3c5137f40
cdennis3223 <carldennis3223@gmail.com>
2026-03-23 14:47:48 -0700

    updated Image processing function to resize

--- ImageParameters.py
+++ ImageParameters.py
@@ -16,13 +16,28 @@ TestDir = DataDir + "/Testing"
 TrainDS = ImageFolder(TrainDir)
 TestDS = ImageFolder(TestDir)
 
-def LocalContrastBoost(img, GridSize, BoostFactor):
-    #Convert to Grayscale
-    img = np.mean(img, axis=2)
+plt.close('all')
 
+def PreProcessImg(img, GridSize, BoostFactor, TargetSize):
+    # Convert to grayscale
+    img = np.mean(img, axis=2)
+    
+    #Resize the imgae
+    target_h, target_w = TargetSize
+    h,w = img.shape
+    scale = min(target_h/h, target_w/w)
+    new_h, new_w = int(h*scale), int(w*scale)
+    img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+    
+    #Pad the image to the target size
+    padded = np.zeros((target_h, target_w), dtype=img.dtype)
+    y_offset = (target_h - new_h) // 2
+    x_offset = (target_w - new_w) // 2
+    padded[y_offset:y_offset+new_h, x_offset:x_offset+new_w] = img
+    img=padded
     #Normalize the imgae
     img = (img - np.mean(img)) / (np.std(img) + 1e-8)
-    #Cast to uint8 for CLAHE, Contrast boosting
+    #Cast to uint8 Contrast boosting
     img = img-img.min()
     img = img/(img.max() + 1e-8)
     img = (img * 255).astype(np.uint8)
@@ -37,7 +52,7 @@ def LocalContrastBoost(img, GridSize, BoostFactor):
             #Extract the current grid
             grid = img[i:i+grid_h, j:j+grid_w]
             #Calculate the mean intensity of the grid
-            mean_intensity = np.mean(grid)
+            mean_intensity = np.mean(grid)                
             #Boost the contrast by scaling pixel values based on their distance from the mean
             boosted_grid = (grid - mean_intensity) * BoostFactor + mean_intensity
             #Clip pixel values to valid range [0, 255]
@@ -47,13 +62,24 @@ def LocalContrastBoost(img, GridSize, BoostFactor):
     return boosted_img.astype(np.uint8)
 
 for i in range(6):
-    random.seed(i)
+    random.seed(i+8)
     idx = random.randint(0, len(TrainDS)-1)
     img, label = TrainDS[idx]
-    img = LocalContrastBoost(img, GridSize=(8, 8), BoostFactor=1.5)
+    img = PreProcessImg(img, GridSize=(10, 10), BoostFactor=1.75, TargetSize=(256, 256))
+    plt.figure(1)
     plt.subplot(2,3,i+1)
     plt.imshow(img, cmap='gray')
     plt.title(TrainDS.classes[label])
 plt.show()
 
+for i in range(6):
+    random.seed(i+8)
+    idx = random.randint(0, len(TrainDS)-1)
+    img, label = TrainDS[idx]
+    #img = PreProcessImg(img, GridSize=(10, 10), BoostFactor=1.5, TargetSize=(256, 256))
+    plt.figure(2)
+    plt.subplot(2,3,i+1)
+    plt.imshow(img, cmap='gray')
+    plt.title(TrainDS.classes[label])
+plt.show()
     
